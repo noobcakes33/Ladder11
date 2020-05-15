a93be4cb2eb5369f9fe12b9987fb5ede993c0b71
noobcakes33 <noobcakes33@gmail.com>
2020-05-15 12:14:16 +0200

    ladder problems

--- 186A_comparing_strings.py
+++ 186A_comparing_strings.py
@@ -1,49 +1,3 @@
-"""
-g1 = input()
-g2 = input()
-
-if g1 == g2:
-    print("YES")
-elif len(g1) != len(g2):
-    print("NO")
-else:
-    g1 = [i for i in g1]
-    g2 = [i for i in g2]
-    no_flag = 1
-    for i in range(len(g1)-1):
-        tmp = g1[i]
-        g1[i] = g1[i+1]
-        g1[i+1] = tmp
-        if g1 == g2:
-            print("YES")
-            no_flag = 0
-            break
-    if no_flag == 1:
-        print("NO")
-"""
-"""
-def compare_strings(s1, s2):
-    s1 = input()
-    s2 = input()
-    s1 = [i for i in s1]
-    s2 = [i for i in s2]
-    match = []
-    for i, j in zip(s1, s2):
-        match.append(int(i==j))
-    c = match.count(0)
-    if c == 0:
-        return "YES"
-    elif c != 2:
-        return "NO"
-    elif c == 2:
-        mismatch = [idx for idx in range(len(match)) if match[idx] == 0]
-        if abs(mismatch[0] - mismatch[1]) == 1:
-            return "YES"
-        else:
-            return "NO"
-
-print(compare_strings("abab", "baba"))
-"""
 s1 = input()
 s2 = input()
 s1 = [i for i in s1]
@@ -67,6 +21,3 @@ if len(s1) == len(s2):
             print("NO")
 else:
     print("NO")
-    #rtfabanpc
-
-#atfabrnpc
